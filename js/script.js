7c97411a2e3a2d0cbba3d75599aa16d867b32dc0
thapabikash48@gmail.com <thapabikash48@gmail.com>
2018-09-26 17:58:23 +0530

    Input Field updated

--- js/script.js
+++ js/script.js
@@ -7,4 +7,25 @@ $(document).ready(function(){
 
 
     });
+
+
+    $('.btn-std').click(function(){
+        var i = 0;
+        var input  = $('<p><input type="text" name="std_name'+i+'" id="std_name'+i+'" placeholder="Full Name"> '
+            +'<input type="text" name="std_usn'+i+'" id="std_usn'+i+'" placeholder="USN"></p>');
+        $('#project-team').append(input);
+    });
+
+
+    $('.btn-tch').click(function(){
+        var i = 0;
+        var input  = $('<p><input type="text" name="tch_name'+i+'" id="tch_name'+i+'" placeholder="Full Name"> '
+            +'<input type="text" name="tch_post'+i+'" id="tch_post'+i+'" placeholder="Desigination"></p>');
+        $('#project-mentors').append(input);
+    });
+
+
+
 });
+
+
